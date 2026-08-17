efca5f59ac22202ecd85046f871cc2b083f18ec5
Dapigman <154299515+dapigman2003@users.noreply.github.com>
2026-08-17 22:30:36 +0000

    clear out junk

--- ManagedPreparationGate.cs
+++
@@ -1,9 +0,0 @@
-namespace StS2Launcher.Core;
-
-public enum ManagedPreparationGate
-{
-    FixtureRead = 1,
-    FixtureRoundTrip = 2,
-    ControlledIlRewrite = 3,
-    RealStS2MetadataInspection = 4,
-}
