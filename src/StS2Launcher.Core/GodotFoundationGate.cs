d84da233c6e7181617c34d5571ddc2cfc3b90a04
Dapigman <154299515+dapigman2003@users.noreply.github.com>
2026-08-17 16:59:38 -0500

    Delete src directory

--- src/StS2Launcher.Core/GodotFoundationGate.cs
+++
@@ -1,9 +0,0 @@
-namespace StS2Launcher.Core;
-
-public enum GodotFoundationGate
-{
-    NativeAvailability = 1,
-    EngineInitializeRenderLoop = 2,
-    MetalRender = 3,
-    TouchLifecycle = 4,
-}
