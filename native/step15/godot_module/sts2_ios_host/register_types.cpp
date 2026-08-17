e02e732d9706f014d0f7d23f7e6e096de504e2d2
Dapigman <154299515+dapigman2003@users.noreply.github.com>
2026-08-17 12:59:27 -0500

    Delete native/step15 directory

--- native/step15/godot_module/sts2_ios_host/register_types.cpp
+++
@@ -1,9 +0,0 @@
-#include "register_types.h"
-
-void initialize_sts2_ios_host_module(ModuleInitializationLevel p_level) {
-    (void)p_level;
-}
-
-void uninitialize_sts2_ios_host_module(ModuleInitializationLevel p_level) {
-    (void)p_level;
-}
