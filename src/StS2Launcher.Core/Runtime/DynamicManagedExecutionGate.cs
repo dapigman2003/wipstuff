ab034285358459966edc561d4d11620e5b9329f1
Dapigman <154299515+dapigman2003@users.noreply.github.com>
2026-08-19 13:52:36 -0500

    Delete src directory

--- src/StS2Launcher.Core/Runtime/DynamicManagedExecutionGate.cs
+++
@@ -1,9 +0,0 @@
-namespace StS2Launcher.Core;
-
-public enum DynamicManagedExecutionGate
-{
-    FixtureIntegrityAndOfflineReady = 1,
-    DynamicFixtureExecution = 2,
-    PrivateDependencyResolution = 3,
-    IsolationAudit = 4,
-}
--- src/StS2Launcher.Core/Runtime/RuntimeFrameworkBindingGate.cs
+++
@@ -1,9 +0,0 @@
-namespace StS2Launcher.Core;
-
-public enum RuntimeFrameworkBindingGate
-{
-    RuntimePayloadClassification = 1,
-    HostFrameworkBindingPlan = 2,
-    PreparedRuntimeAssemblySet = 3,
-    ClosureAudit = 4,
-}
